9334ab73a9466cc4b6a5f9247d1b1026dc2401b2
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2022-10-13 16:36:56 +0000

    layout and button Component

--- src/layout/AuthScreens.vue
+++ src/layout/AuthScreens.vue
@@ -1,85 +1,21 @@
 <template>
   <div class="container">
     <div class="sub-container">
-      <nav>
         <figure>
           <slot name="image">
             <img :src="logo" alt="logo" />
           </slot>
         </figure>
-      </nav>
       <div class="main">
         <div class="left">
           <div>
-            <h1>
-              <slot name="title">Create Your Account</slot>
-            </h1>
-            <p>
-              <slot name="paragraph">Let's get started</slot>
-            </p>
+              <h1 class="title">{{title}}</h1>
+              <p class="subtitle">{{subtitle}}</p>
           </div>
         </div>
-        <div class="right">
-          <div class="form-container">
-            <form>
-              <div class="id-flex">
-                <div>
-                  <label>
-                    <slot name="firstName">First Name</slot>
-                  </label>
-                  <slot name="input">
-                    <div class="styled-input">
-                      <input type="text" />
-                    </div>
-                  </slot>
-                </div>
-                <div>
-                  <label>
-                    <slot name="lastName">Last Name</slot>
-                  </label>
-                  <slot name="input">
-                    <div class="styled-input">
-                      <input type="text" />
-                    </div>
-                  </slot>
-                </div>
-              </div>
-              <label>
-                <slot name="email">Email Address</slot>
-              </label>
-              <slot name="input">
-                <div class="styled-input">
-                  <input type="text" />
-                </div>
-              </slot>
-              <label>
-                <slot name="email">Phone Number</slot>
-              </label>
-              <slot name="input">
-                <div class="styled-input">
-                  <input type="text" />
-                </div>
-              </slot>
-              <label>
-                <slot name="email">Password</slot>
-              </label>
-              <slot name="input">
-                <div class="styled-input">
-                  <input type="text" />
-                </div>
-              </slot>
-              <slot name="button">
-                <button class="btn">Create Account</button>
-              </slot>
-              <slot name="login">
-                <p class="para">
-                  already have an account
-                  <span><a href="#" class="sign-in">Sign In</a></span>
-                </p>
-              </slot>
-            </form>
+          <div class="right" >
+            <slot name="content"></slot>
           </div>
-        </div>
       </div>
     </div>
   </div>
@@ -89,7 +25,17 @@
 import logo from "@/assets/Union.svg";
 export default {
   name: "authDashboard",
+    props: {
+        title: {
+            type: String,
+            default: "Create An Account"
+        },
+        subtitle:{
+            type: String,
+            default: "Let's get started"
+        }
 
+    },
   data() {
     return {
       logo,
@@ -111,62 +57,12 @@ export default {
   padding: 50px 0px 0px 0px;
 }
 
-nav{
-    margin-bottom: 10px;
-}
-
 .main {
   display: flex;
   justify-content: space-between;
 }
 
-.form-container {
-  background: #ffffff;
-  border: 1px solid #dee3eb;
-  border-radius: 8px;
-  padding: 40px 32px;
-}
-
-.id-flex {
-  display: flex;
-  gap: 10px;
-}
-
-label {
-  display: block;
-  font-family: "Inter";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-  letter-spacing: 0.4px;
-  color: #6d7a98;
-}
-
-.styled-input {
-  background: #ffffff;
-  border: 1px solid #dee3eb;
-  border-radius: 4px;
-  margin: 8px 0px 24px 0px;
-}
-
-input {
-  width: 100%;
-  border: none;
-  padding: 16px;
-}
-
-.btn {
-  border: none;
-  background: #1f6aec;
-  border-radius: 8px;
-  width: 100%;
-  padding: 12px 0px;
-  color: #ffffff;
-  margin-bottom: 24px;
-}
-
-.para {
+.subtitle{
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
@@ -175,11 +71,6 @@ input {
   flex: none;
   order: 5;
   flex-grow: 0;
-}
-
-.sign-in {
-  color: #1f6aec;
-  text-decoration: none;
 }
 
 .left{
