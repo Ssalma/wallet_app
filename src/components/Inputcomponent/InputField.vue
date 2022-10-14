d6a711b4ff4c163b97ca98597466e78ddfd54b58
user <user@users-MacBook-Pro.local>
2022-10-14 10:41:07 +0000

    inputFields update

--- src/components/Inputcomponent/InputField.vue
+++ src/components/Inputcomponent/InputField.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>
-    <label>{{ label }}</label>
+  <div class="main-wrapper">
+    <label :class="['label__text']">{{ label }}</label>
+    <div class="input-wrapper">
+      <input
+        v-bind="$attrs"
+        placeholder=""
+        :type="type === 'password' ? innerType : type"
+        :value="modelValue"
+        :class="['input__item']"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
 
-    <input v-bind="$attrs" placeholder="" :value="modalValue" />
+      <div
+        v-if="type === 'password'"
+        class="eye-toggle"
+        @click="toggleInputIcon"
+      >
+        <div v-if="visibility" class="eye-open">
+          <img src="@/assets/eye-open.svg" alt="" srcset="" />
+        </div>
+        <div v-else class="eye-close">
+          <img src="@/assets/eye-closed.svg" alt="" srcset="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,29 +34,73 @@ export default {
       type: String,
       default: "",
     },
-    modalValue: {
+    modelValue: {
       type: [String, Number],
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      visibility: false,
+      innerType: "",
+    };
+  },
+  created() {
+    this.innerType = this.type;
+  },
+  methods: {
+    toggleInputIcon() {
+      this.visibility = !this.visibility;
+      if (this.visibility == true) {
+        this.innerType = "text";
+      } else {
+        this.innerType = "password";
+      }
     },
   },
 };
 </script>
 
-<style>
-input {
-  height: 48px;
-  width: 384px;
-  left: 0px;
-  top: 24px;
-  border-radius: 4px;
-  padding: 10px;
-}
+<style scoped lang="scss">
+.main-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 
-label {
-  display: block;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  letter-spacing: 0.4px;
-  color: #6d7a98;
+  .input-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    position: relative;
+    width: 100%;
+    max-width: 465px;
+    .eye-toggle {
+      position: absolute;
+      right: 0;
+      top: 14px;
+      cursor: pointer;
+      padding-right: 10px;
+    }
+  }
+
+  .input__item {
+    height: 48px;
+
+    border-radius: 4px;
+    padding: 10px;
+    border: 1px solid #dee3eb;
+  }
+  .label__text {
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0.4px;
+    color: #6d7a98;
+    text-align: left;
+  }
 }
 </style>
