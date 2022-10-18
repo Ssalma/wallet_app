291858c366007a0b60f7fb8c58c27eafe53fc532
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2022-10-18 10:57:28 +0000

    ForgetPassword vue and LoginView

--- src/views/auth/login/LoginView.vue
+++ src/views/auth/login/LoginView.vue
@@ -1,22 +1,80 @@
 <template>
-  <auth-screen title="home" subtitle="gdggjhm">
+  <auth-screen title="Log In" subtitle="Great to have you back">
     <template #content>
-      <h1>this</h1>
+      <form>
+        <input-field
+          v-model="emailAddress"
+          label="Email Address"
+          type="email"
+        />
+        <input-field v-model="user.password" label="Password" type="password" />
+        <div>
+          <!-- router-link goes below the button without the div -->
+          <router-link to="/auth/forgetpassword">Forget Password?</router-link>
+        </div>
+        <login-btn buttonTxt="Log In" class="btn--blue"></login-btn>
+        <p class="para">
+          Don't have an account? <router-link to="/auth/signup">Register now</router-link>
+        </p>
+      </form>
     </template>
   </auth-screen>
 </template>
 
 <script>
-import AuthScreen from '@/layout/AuthScreens.vue'
-
+import LoginBtn from "@/components/button/ButtonComponent.vue";
+import AuthScreen from "@/layout/AuthScreens.vue";
+import InputField from "@/components/Inputcomponent/InputField.vue";
 export default {
-    components: {
-        AuthScreen 
-    },
-
-}
+  components: {
+    AuthScreen,
+    InputField,
+    LoginBtn,
+  },
+  data() {
+    return {
+      user: {},
+    };
+  },
+};
 </script>
 
-<style>
+<style scoped>
+form {
+  background: #ffffff;
+  border: 1px solid #dee3eb;
+  border-radius: 8px;
+  padding: 40px 32px;
+  min-width: 528px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
 
+a {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  color: #1f6aec;
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+  text-decoration: none;
+}
+
+.para {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 0.4px;
+  color: #6d7a98;
+}
+
+@media (max-width: 375px){
+  form{
+    min-width: 328px;
+  }
+}
 </style>
